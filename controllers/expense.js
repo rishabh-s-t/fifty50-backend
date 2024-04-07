9d633b3e5a431572fd34351267e64e5ea4134dab
APPLE <apple@Rishabhs-Macbook.local>
2024-04-07 13:26:33 +0530

    final functionalities

--- controllers/expense.js
+++ controllers/expense.js
@@ -5,114 +5,126 @@ const mongoose = require('mongoose');
 const { calculateSplit } = require('../services/expenseService');
 
 const addExpenseController = async (req, res) => {
-    try {
-        let groupId = req.body.group
-        let title = req.body.title
-        let paidBy = req.body.paidBy
-        let description = req.body.description
-        let amount = +req.body.amount
-        let avatar = req.body.avatar
-        let members = req.body.members
+  try {
+    let groupId = req.body.group;
+    let title = req.body.title;
+    let paidBy = req.body.paidBy;
+    let description = req.body.description;
+    let amount = +req.body.amount;
+    let avatar = req.body.avatar;
+    let members = req.body.members;
 
-        if (!avatar) avatar = 1
-        if (!description) description = ''
+    if (!avatar) avatar = 1;
+    if (!description) description = '';
 
-        let group = await Group.findById(groupId)
+    let group = await Group.findById(groupId);
 
-        if (!group) {
-            res.status(404).send('Group not found')
-        }
-
-        let memberBalances = await calculateSplit(paidBy, members, amount)
-
-        const expense = new Expense({
-            title: title,
-            avatar: avatar,
-            description: description,
-            amount: amount,
-            date: Date.now(),
-            group: groupId,
-            paidBy: paidBy,
-            members: members,
-            membersBalance: memberBalances,
-            settledMembers: [paidBy],
-            isSettled: false
-        })
-
-        await expense.save()
-        res.status(200).send({
-            message: 'expense added successfully',
-            expense: expense,
-        })
-    } catch (error) {
-        res.status(400).send({
-            message: "Ran into an error while contacting DB",
-            error: error.message,
-        })
-        return
+    if (!group) {
+      res.status(404).send('Group not found');
     }
-}
+
+    let memberBalances = await calculateSplit(paidBy, members, amount);
+
+    const expense = new Expense({
+      title: title,
+      avatar: avatar,
+      description: description,
+      amount: amount,
+      date: Date.now(),
+      group: groupId,
+      paidBy: paidBy,
+      members: members,
+      membersBalance: memberBalances,
+      settledMembers: [paidBy],
+      isSettled: false,
+    });
+
+    await expense.save();
+    res.status(200).send({
+      message: 'expense added successfully',
+      expense: expense,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: 'Ran into an error while contacting DB',
+      error: error.message,
+    });
+    return;
+  }
+};
 
 const getGroupMemberExpenseController = async (req, res) => {
-    const groupId = req.params.groupId
-    const memberId = req.params.memberId
+  const groupId = req.params.groupId;
+  const memberId = req.params.memberId;
 
-    const expenses = await Expense.find({
-        group: groupId,
-    }).populate('paidBy', {
-        name: 1,
-        _id: 1
-    })
+  const expenses = await Expense.find({
+    group: groupId,
+  }).populate('paidBy', {
+    name: 1,
+    _id: 1,
+  });
 
-    const activeExpenses = expenses.filter(expense => {
-        return (
-            expense.settledMembers.indexOf(memberId) === -1 && !expense.isSettled
-        )
-    })
+  const activeExpenses = expenses.filter((expense) => {
+    return (
+      expense.settledMembers.indexOf(memberId) === -1 && !expense.isSettled
+    );
+  });
 
-    const settledExpenses = expenses.filter(expense => {
-        return expense.settledMembers.indexOf(memberId) > -1 || expense.isSettled
-    })
+  const settledExpenses = expenses.filter((expense) => {
+    return expense.settledMembers.indexOf(memberId) > -1 || expense.isSettled;
+  });
 
-    res.send({
-        activeExpenses,
-        settledExpenses
-    })
-}
+  res.send({
+    activeExpenses,
+    settledExpenses,
+  });
+};
 
 const updateMemberExpense = async (req, res) => {
-    const expenseId = req.params.expenseId
-    const memberId = req.params.memberId
-    const expense = await Expense.findById(expenseId)
+  const expenseId = req.params.expenseId;
+  const memberId = req.params.memberId;
+  const expense = await Expense.findById(expenseId);
 
-    if (!expense) res.status(400).send('Expense not found!')
+  if (!expense) {
+    return res.status(400).send('Expense not found!');
+  }
 
-    const index = expense.settledMembers.indexOf(memberId)
+  const index = expense.settledMembers.indexOf(memberId);
 
-    if (index > -1) res.status(200).send({
-        message: 'Member already settled',
-        expense: expense,
-    })
-    else expense.settledMembers.push(memberId)
+  if (index > -1) {
+    return res.status(200).send({
+      message: 'Member already settled',
+      expense: expense,
+    });
+  } else {
+    expense.settledMembers.push(memberId);
+  }
 
-    if (expense.settledMembers.length === 0) {
-        expense.isSettled = false;
-    } else {
-        const paidByString = expense.paidBy.toString();
+  const memberIndex = expense.membersBalance.findIndex(
+    (member) => member.id == memberId
+  );
+  if (memberIndex !== -1) {
+    expense.membersBalance[memberIndex].balance = `${Math.abs(
+      expense.membersBalance[memberIndex].balance
+    )}`;
+  }
 
-        const unSettledMembers = expense.membersBalance.filter(member => member.id.toString() !== paidByString);
+  const paidByString = expense.paidBy.toString();
+  const unSettledMembers = expense.membersBalance.filter(
+    (member) => member.id.toString() !== paidByString
+  );
+  if (expense.settledMembers.length === unSettledMembers.length) {
+    expense.isSettled = true;
+  } else {
+    expense.isSettled = false;
+  }
 
-        if (expense.settledMembers.length === unSettledMembers.length) {
-            expense.isSettled = true;
-        }
-    }
-
-    await expense.save()
-    res.send(expense)
-}
+  await expense.save();
+  res.send(expense);
+};
 
 module.exports = {
-    addExpenseController,
-    getGroupMemberExpenseController,
-    updateMemberExpense
+  addExpenseController,
+  getGroupMemberExpenseController,
+  updateMemberExpense,
 };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const colors = require("colors");
-const morgan = require("morgan");
-const connectDB = require("./config/db");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //DOTENV
 dotenv.config();
@@ -17,18 +17,18 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 //ROUTES
 //User Routes
-app.use('/api/v1/user', require('./routes/user'))
-app.use('/api/v1/group', require('./routes/group'))
-app.use('/api/v1/expense', require('./routes/expense'))
+app.use('/api/v1/user', require('./routes/user'));
+app.use('/api/v1/group', require('./routes/group'));
+app.use('/api/v1/expense', require('./routes/expense'));
 
 //PORT
 const PORT = process.env.PORT || 8080;
 
 //listen
 app.listen(PORT, () => {
-    console.log(`Server Runnning ${PORT}`.bgGreen.white);
+  console.log(`Server Runnning ${PORT}`.bgGreen.white);
 });
